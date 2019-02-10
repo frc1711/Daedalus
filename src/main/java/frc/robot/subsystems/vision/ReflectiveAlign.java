a977a595a9d9a5f4b78b670fa36d28867abc1875
sedezee <sedezee@gmail.com>
2019-02-10 17:12:32 -0500

    Added possible method for lining up

---
+++ src/main/java/frc/robot/subsystems/vision/ReflectiveAlign.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems.vision;
+
+
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
+/**
+ * Add your docs here.
+ */
+
+public class ReflectiveAlign {
+    private static ArrayList<Block> blocks = CameraConfig.getPixyCamera().getPixy().getCCC().getBlocks();
+    private static final int blockSignature = 1; 
+    public static Block largestBlock = null; 
+    public static Block smallestBlock = null; 
+    public static double yaw; 
+
+    public static void run(int count) {
+        if (count > 0 ) {
+            Block largestBlock = null; 
+            Block smallestBlock = null; 
+            for (Block block : blocks) {
+                if(block.getSignature() == blockSignature) {
+                    //i think this needs to be changed but who knows
+                    if (largestBlock == null)
+                        largestBlock = block; 
+                    else if (block.getWidth() > largestBlock.getWidth()) 
+                        largestBlock = block;
+                    else if (block.getWidth() < largestBlock.getWidth())
+                        smallestBlock = block; 
+                } 
+            }
+
+            int tapeXLarge = largestBlock.getX(); 
+            int tapeXSmall = smallestBlock.getX(); 
+            int averageX = (tapeXLarge + tapeXSmall) / 2; 
+            double yaw = ((averageX - 157.5) * 0.1904761905);
+
+            SmartDashboard.putNumber("Large X, Tape", tapeXLarge); 
+            SmartDashboard.putNumber("Small X, Tape", tapeXSmall); 
+            SmartDashboard.putNumber("Average X, Tape", averageX);
+            SmartDashboard.putNumber("Tape Angle?", yaw); 
+            SmartDashboard.putNumber("Large Tape Width", largestBlock.getWidth()); 
+            SmartDashboard.putNumber("Small Tape Width", smallestBlock.getWidth());
+            SmartDashboard.putNumber("Large Tape Height", largestBlock.getHeight());
+            SmartDashboard.putNumber("Smalal Tape Height", smallestBlock.getHeight()); 
+        }
+    }
+}
