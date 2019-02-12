7cbb1467564c48335cde9f0f3667cf2947c6bb4d
sedezee <sedezee@gmail.com>
2019-02-12 13:12:07 -0500

    Removed reflective tape alignment code for now

--- src/main/java/frc/robot/subsystems/vision/ReflectiveAlign.java
+++ src/main/java/frc/robot/subsystems/vision/ReflectiveAlign.java
@@ -17,21 +17,23 @@ import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
  */
 
 public class ReflectiveAlign {
-    private static ArrayList<Block> blocks = CameraConfig.getPixyCamera().getPixy().getCCC().getBlocks();
+   /* private static ArrayList<Block> blocks = CameraConfig.getPixyCamera().getPixy().getCCC().getBlocks();
     private static final int blockSignature = 1; 
     public static Block largestBlock = null; 
     public static Block smallestBlock = null; 
     public static double yaw; 
 
     public static void run(int count) {
-        if (count > 0 ) {
+       /* if (count > 0 ) {
             Block largestBlock = null; 
             Block smallestBlock = null; 
             for (Block block : blocks) {
                 if(block.getSignature() == blockSignature) {
                     //i think this needs to be changed but who knows
-                    if (largestBlock == null)
+                    if (largestBlock == null && smallestBlock == null) {
                         largestBlock = block; 
+                        smallestBlock = block; 
+                    }
                     else if (block.getWidth() > largestBlock.getWidth()) 
                         largestBlock = block;
                     else if (block.getWidth() < largestBlock.getWidth())
@@ -39,13 +41,14 @@ public class ReflectiveAlign {
                 } 
             }
 
+            
             int tapeXLarge = largestBlock.getX(); 
             int tapeXSmall = smallestBlock.getX(); 
             int averageX = (tapeXLarge + tapeXSmall) / 2; 
             double yaw = ((averageX - 157.5) * 0.1904761905);
 
-            SmartDashboard.putNumber("Large X, Tape", tapeXLarge); 
-            SmartDashboard.putNumber("Small X, Tape", tapeXSmall); 
+            SmartDashboard.putNumber("Large X, Tape", largestBlock.getX()); 
+            SmartDashboard.putNumber("Small X, Tape", smallestBlock.getX()); 
             SmartDashboard.putNumber("Average X, Tape", averageX);
             SmartDashboard.putNumber("Tape Angle?", yaw); 
             SmartDashboard.putNumber("Large Tape Width", largestBlock.getWidth()); 
@@ -53,5 +56,26 @@ public class ReflectiveAlign {
             SmartDashboard.putNumber("Large Tape Height", largestBlock.getHeight());
             SmartDashboard.putNumber("Smalal Tape Height", smallestBlock.getHeight()); 
         }
-    }
+    } 
+    if (count > 0) {
+        Block largestBlock = null;  
+        for (Block block : blocks) {
+          if(block.getSignature() == blockSignature) {
+            if (largestBlock == null) 
+              largestBlock = block; 
+            else if (block.getWidth() > largestBlock.getWidth()) 
+              largestBlock = block; 
+          }
+        }
+  
+        int ballX = largestBlock.getX();
+        double yaw = ((ballX - 157.5) * 0.1904761905);
+  
+        SmartDashboard.putNumber("Ball Angle", yaw);
+        SmartDashboard.putNumber("Ball X", largestBlock.getX());
+        SmartDashboard.putNumber("Ball Y", largestBlock.getY());
+        SmartDashboard.putNumber("Ball Box Width", largestBlock.getWidth());
+        SmartDashboard.putNumber("Ball Box Height", largestBlock.getHeight());
+      }
+    } */
 }
